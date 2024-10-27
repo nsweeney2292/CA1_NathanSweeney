ee38e45cde05ee195d4eb7e2a8b90af0cad86cc5
nsweeney2292 <sba24204@student.cct.ie>
2024-10-27 10:37:36 +0000

    Added imports, used bufferedreader to read file, used parse for data types

--- src/ca1_nathansweeney/CA1_NathanSweeney.java
+++ src/ca1_nathansweeney/CA1_NathanSweeney.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ca1_nathansweeney;
+//More efficient to read files in conjunction with FileReader
+import java.io.BufferedReader; 
+// Reads character data from file - for eg our customers.txt
+import java.io.FileReader;
 
 /**
  *
@@ -15,6 +19,33 @@ public class CA1_NathanSweeney {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String filename = "customers.txt";
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))){
+            String line;
+            
+            while ((line = br.readLine()) != null){
+                //customers name - first and last - String
+                String custName = line.trim();
+                
+                //total purchase value - double
+                // converts to double 
+                line = br.readLine();
+                double purchase = Double.parseDouble(line.trim());
+                
+                //class of the customer - int
+                //converts to int
+                line = br.readLine();
+                int custClass = Integer.parseInt(line.trim());
+                
+                //purchase year - int
+                //converts to int
+                line = br.readLine();
+                int year = Integer.parseInt(line.trim());
+                
+                //create customer class and add instance below
+            }
+        }//add catch
     }
     
 }
